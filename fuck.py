0eaf5414b13ab5675f2763cba8d4e48b08bc5da9
leehoon3722 <leehoon3722@gmail.com>
2026-05-09 00:59:12 +0900

    Update fuck.py

--- fuck.py
+++ fuck.py
@@ -3,6 +3,17 @@ import dlib
 import numpy as np
 from scipy.spatial import distance as dist
 
+def gstreamer_pipeline(sensor_id=0, capture_width=1280, capture_height=720, display_width=640, display_height=360, framerate=30, flip_method=0):
+    return (
+        "nvarguscamerasrc sensor-id=%d ! "
+        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! "
+        "nvvidconv flip-method=%d ! "
+        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
+        "videoconvert ! "
+        "video/x-raw, format=(string)BGR ! appsink"
+        % (sensor_id, capture_width, capture_height, framerate, flip_method, display_width, display_height)
+    )
+
 def calculate_ear(eye):
     A = dist.euclidean(eye[1], eye[5])
     B = dist.euclidean(eye[2], eye[4])
@@ -19,7 +30,7 @@ predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
 (lStart, lEnd) = (42, 48)
 (rStart, rEnd) = (36, 42)
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(gstreamer_pipeline(sensor_id=0), cv2.CAP_GSTREAMER)
 counter = 0
 
 while True:
